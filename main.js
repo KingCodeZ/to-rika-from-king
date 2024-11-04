b2b0a5c3d61059cd42379c02287484f12b35e5d1
KingCodeZ <160741812+KingCodeZ@users.noreply.github.com>
2024-11-04 20:31:56 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+var app = document.getElementById('app')
+
+var Typewriter = new Typewriter(app, {
+    loop: true,
+    delay : 55,
+});
+
+Typewriter
+    .pauseFor(2000)
+    .typeString('Hey Mirabel ')
+    .pauseFor(300)
+    .deleteChars(20)
+    .typeString(" By the time you read this you will be officially 18 and i'll prolly be down w a cold.. ")
+    // .deleteChars(161)
+    .pauseFor(4000)
+    .typeString(" Feels like it's getting worse butttt, yep, 18 years of age...the big 1 8  you're officially legal, an adult, you can vote, you can drink..legally😂,get arrested too ")
+    .typeString(" ...though we both know your ass ain't leaving home😂 ,unless maybe under strict supervision or sumn, you're officially one more year to the end of the teenage years.... ")
+    .typeString(" we're growing up fast, feels like yesterday i was getting roasted by a pretty SS1 girl in my school bus for being too...idk...indomie i guess😂 ")
+    .typeString(" and now i'm writing an epistle wishing her, my bestie a happy 18th birthday, life innit🙂‍↕️             ")
+    .pauseFor(4000)
+
+    .typeString(" Rika, i'm glad we're friends, i really am, it's one of the things in life i'm honestly really grateful for. ")
+    .typeString("Thank you, for every laugh, every 3 hours straight texting😂, thanks for checking up on me, asking how i am, how's the project goin(no ask abeg as you read this)....")
+    .typeString(", how's life, that time i had exams, jamb and the post utme screenings, thanks for the prayers and encouragements and reminders to read...(reminders i might have not responded to in time buttttttt we up),")
+    .typeString(" everything, this year honestly had a lotta highs and lows and might not be the most fav year of mine but seeing your notification pop up on my screen had to be one of the highlights of this shitty year,")
+    .typeString("  thanks for being there for me when 4 schools i wanted to go to didn't work out consecutively, thanks for bringing up the whole purge thing and thanks for not going crazy on me hwen you found out bout my absolutely breaking the purge ♥️<br>")
+    .typeString(" i feel there's more i haven't said but can't remember rn but damn, thanks for everything. thanks for existing<br>")
+   
+    .typeString('<strong style="color: goldenrod;">Now to prayers</strong> <br>')
+    .pauseFor(300)
+
+    .typeString("I pray for Good Health, i really really pray for good health cause the caffeine intake eh...May your sleep be restful and helpful asf, yes, even the 2/ 4 hr ones😂, ")
+    .typeString(" and your energy levels through the roof and ability to stay up effective as always, though your body might be adapting to caffeine but omooo i pray you do. Here’s to no sudden colds or fevers, no more headaches, by God's grace, awfully painful cramps(them go still show but hey),")
+    .typeString("imma pray for more stability maybe😭...speaking of health")
+    .typeString(" I pray you smash everything, like every single thing, from classworks to assignments to projects, most of all, the exams, from anatomy to the other crazy ahh ones 😭 but fr...like a whole new wave of academic success like never seen before, i hope every exam is a breeze and every practical,")
+    .typeString(" class, everything, works out perfectly well. Most of all, i pray you hardwork pays off, like fr, no one i know reads harder, downs coffee and energy drinks harder and puts in the work as much as you, i've said this before and i'll keep saying it, babe, you absolutetly inspire me and i will always look at you in awe frrr.")
+    .typeString(" I pray when it's all said and done by God's grace, you get the all opportunities you deserve. nerd🌚")
+    .pauseFor(300)
+    .typeString(" I wish you all the happiness in the world and more. May you always find reasons to smile, even on tough days. I pray you’re surrounded by love and positivity always, and whenever life is a bitch, it's a bad one🌚, gfb i hope it's not but when it is, we gonn flip that shii over, ight?🌚 ")
+    .typeString(" I also pray you have a better relationship w God, know him better, figure out his plan for you and he blesses you. i pray you continue to grow n keep being the you, the fucking KING you are. i also pray for good memories, better life experiences and all you pray for and wish yourself. and most of all,")
+    .typeString(" i pray you keep being you, my wonderful, beautiful, smart, caffeine and ibrupofen(idk the spelling) using, nerdy bestie and you keep evolvong and reaching greater heights in life and everything .Remember, I’m always here for you. Whether you need a face to put make-up on, a workout partner, or just someone to rant to after a long day, send hour long vns maybe, I’ve got your back. fuckin Love you Babe. Happy i8th Birthday, here's to many more🥂<br>")
+    .pauseFor(300)
+    .typeString(" <i>Love, Davies</i>")
+
+    .pauseFor(1000)
+    .start()
